81e4bdecde500b0446bfd767b95cd2f0a42205bb
Cristobal Aguirre <caguirre@deetken.com>
2019-07-04 13:50:26 -0700

    adding styled components

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import Nav from './Nav';
 
-const Header = () => {
-  return (
-    <div>
-      <div className="bar">
-        <a href="">Sick Fits</a>
-      </div>
-      <Nav />{' '}
-      <div className="sub-bar">
-        <p>Search</p>
-      </div>
-      <div>Cart</div>
+const Header = () => (
+  <div>
+    <div className="bar">
+      <a href="">Sick Fits</a>
     </div>
-  );
-};
+    <Nav />
+    <div className="sub-bar">
+      <p>Search</p>
+    </div>
+    <div>Cart</div>
+  </div>
+);
 
 export default Header;
--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import Header from '../components/Header';
-import Meta from '../components/Meta';
+import styled from 'styled-components';
+import Header from './Header';
+import Meta from './Meta';
+
+const StyledPage = styled.div`
+  background: white;
+  color: black;
+`;
+
+const Inner = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 class Page extends React.Component {
   render() {
+    const { children } = this.props;
     return (
-      <div>
+      <StyledPage>
         <Meta />
         <Header />
-        {this.props.children}
-      </div>
+        {children}
+      </StyledPage>
     );
   }
 }
